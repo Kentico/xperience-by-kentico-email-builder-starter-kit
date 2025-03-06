27ca41e6949a77e155e7651108fc1c4e22accd36
bkapustik <boriskokap@gmail.com>
2025-03-06 13:31:33 +0100

    feat(css): experimental way of defining classes for the emailbuilder

---
+++ src/Kentico.Xperience.Mjml.StarterKit.Rcl/CssLoaderService.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Hosting;
+
+public class CssLoaderService
+{
+    private readonly IWebHostEnvironment enviroment;
+
+    public CssLoaderService(IWebHostEnvironment enviroment)
+        => this.enviroment = enviroment;
+
+    public async Task<string> GetCssAsync(string relativePath)
+    {
+        string path = System.IO.Path.Combine(enviroment.WebRootPath, relativePath.TrimStart('/'));
+
+        if (File.Exists(path))
+        {
+            string text = await File.ReadAllTextAsync(path);
+            return text.Trim();
+        }
+
+        return "/* CSS not found */";
+    }
+}
