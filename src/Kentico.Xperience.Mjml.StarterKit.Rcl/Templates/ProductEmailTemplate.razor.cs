dea2e8c3ba0b4406ac11e0f66e92f570421b6bab
kapustb <55467298@mff.cuni.cz>
2025-03-31 12:50:27 +0200

    fix(Product template): separate markup and code

--- src/Kentico.Xperience.Mjml.StarterKit.Rcl/Templates/ProductEmailTemplate.razor.cs
+++ src/Kentico.Xperience.Mjml.StarterKit.Rcl/Templates/ProductEmailTemplate.razor.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using Kentico.Xperience.Mjml.StarterKit.Rcl.Sections;
+
+using Microsoft.AspNetCore.Components;
 
 namespace Kentico.Xperience.Mjml.StarterKit.Rcl.Templates;
 
@@ -11,4 +13,10 @@ public partial class ProductEmailTemplate : ComponentBase
     /// The component identifier.
     /// </summary>
     public const string IDENTIFIER = $"Kentico.Xperience.Mjml.StarterKit.{nameof(ProductEmailTemplate)}";
+
+    private string cssContent = string.Empty;
+
+    private readonly string sectionIdentifier = FullWidthEmailSection.IDENTIFIER;
+
+    protected override async Task OnInitializedAsync() => cssContent = await CssLoaderService.GetCssAsync();
 }
